2c86e44c3e2c6e75f2efa4ec2130aa860ca2be3a
Onur Gürkan <onurgurkaann@gmail.com>
2022-03-01 15:08:23 +0300

    javareact first spring project-continuation update

--- javareact/src/main/java/softwaredevcamp/javareact/dataAccess/abstracts/ProductDao.java
+++ javareact/src/main/java/softwaredevcamp/javareact/dataAccess/abstracts/ProductDao.java
@@ -3,6 +3,6 @@ package softwaredevcamp.javareact.dataAccess.abstracts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import softwaredevcamp.javareact.entities.concretes.Product;
 
-public interface ProductDao extends JpaRepository<Product, Integer>{ // -> Table(Product) and Dataype(Integer)
+public interface ProductDao extends JpaRepository<Product, Integer>{ // -> Table(Product) and Data Type(Integer)
 	
 }
